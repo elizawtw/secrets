ad176ff3b2f03258b1d255ffcb0eeff453d46379
eliza <twew02@gmail.com>
2022-11-06 12:53:15 -0500

    initial setup

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const port = 3000 || 3003;
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = {
+  email: String,
+  password: String
+}
+
+const User = new mongoose.model("User", userSchema)
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+  console.log(req.body.username)
+  const newUser = new User ({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err){
+    if(err){
+      console.log(err)
+    } else {
+      res.render("secrets")
+    }
+  })
+});
+
+app.listen(port, ()=>{
+  console.log("Successfully connected to port " + port)
+})
